b7e11f8cb0b4eed5ac72ae0ec76672be1ddf4112
salvatoreFerlante <dev@drauora.it>
2025-10-19 22:38:46 +0200

    Add `Net` module for Wi-Fi connectivity and integrate into main logic

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,30 @@
 #include "SoilSensor.h"
 #include "StatusLED.h"
 #include "Console.h"
+#include "Net.h"
 
 Relay      relay;
 SoilSensor soil;
 StatusLED  led;
 Console    console;
+Net        net;
 
 void setup() {
-  relay.begin(PIN_RELAY, /*activeHigh=*/false);      // se il tuo relè è active-LOW, metti false
+  relay.begin(PIN_RELAY, /*activeHigh=*/true);
   soil.begin(PIN_SOIL_A0, /*samples=*/5);
   led.begin(PIN_LED, /*activeLow=*/true);
-  led.blink(80, 920);                               // heartbeat lento
-  console.begin(115200, &relay, &soil);
+  led.blink(80, 920);
 
+  // === Wi-Fi ===
+  net.begin("esp12e-01", /*portalTimeoutSec=*/180);
+
+  console.begin(115200, &relay, &soil, &net);
   Serial.println("Boot OK. Digita HELP.");
 }
 
 void loop() {
-  relay.update(); // gestisce timeout
-  led.update();      // animazione LED
-  console.update();  // comandi seriali
+  relay.update();
+  led.update();
+  net.update();      // mantiene la connessione
+  console.update();
 }
